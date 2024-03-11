f83cf997ced9ec70787b18cfd0cf1dbafc24f617
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2024-03-11 22:22:11 +0200

    updated profile screen

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -10,7 +10,7 @@ const Loader = ({ signIn, register, update, order }) => {
                 display: "block",
                 width: signIn || register || order || update ? "25px" : "40px",
                 height: signIn || register || order || update ? "25px" : "40px",
-                margin: signIn ? "0 12px -1px 12px" : register ? "0 16px -1px 16px" : update ? "0 13.5px -1px 13.5px" : order ? "0px auto -1px" : "auto"
+                margin: signIn ? "0 12px -1px 12px" : register ? "0 16px -1px 16px" : order || update ? "0px auto -1px" : "auto"
             }}
         >
 
--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Row, Col, Form, Button, Table } from 'react-bootstrap'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Row, Col, Card, Form, Button, Table } from 'react-bootstrap'
 import { useGetMyOrdersQuery } from '../slices/ordersApiSlice'
 import { useProfileMutation } from '../slices/usersApiSlice'
 import { setCredentials } from '../slices/authSlice'
@@ -16,7 +15,6 @@ import Loader from '../components/Loader'
 
 const ProfileScreen = () => {
     const dispatch = useDispatch()
-    const navigate = useNavigate()
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -24,6 +22,7 @@ const ProfileScreen = () => {
     const [updateProfile, { isLoading: loadingUpdateProfile }] = useProfileMutation()
     const { data: orders, isLoading, error } = useGetMyOrdersQuery()
     const { userInfo } = useSelector(state => state.auth)
+    const emptyOrdersList = JSON.stringify(orders) === '[]'
 
     useEffect(() => {
         if (userInfo) {
@@ -49,119 +48,129 @@ const ProfileScreen = () => {
 
     return (
         <>
-            <Row>
-                <Col md={3}>
-                    <h2>Profile</h2>
-                    <Form onSubmit={submitHandler}>
-                        <Form.Group controlId="name" className="my-2">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control
-                                type="name"
-                                value={name}
-                                className="fs-5"
-                                onChange={(event) => setName(event.target.value)}
-                            >
-                            </Form.Control>
-                        </Form.Group>
+            <GoBackButton text="Home" url="/" />
+            <Row className="justify-content-center">
+                <Col md={9} lg={emptyOrdersList ? 5 : 4}>
+                    <Card className="my-3 p-3">
+                        <h3>Personal Details</h3>
+                        <Form onSubmit={submitHandler}>
+                            <Form.Group controlId="name" className="my-2">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control
+                                    type="name"
+                                    value={name}
+                                    className="fs-5"
+                                    onChange={(event) => setName(event.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
 
-                        <Form.Group controlId="email" className="my-3">
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control
-                                type="email"
-                                value={email}
-                                className="fs-5"
-                                onChange={(event) => setEmail(event.target.value)}
-                            >
-                            </Form.Control>
-                        </Form.Group>
+                            <Form.Group controlId="email" className="my-3">
+                                <Form.Label>Email Address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    value={email}
+                                    className="fs-5"
+                                    onChange={(event) => setEmail(event.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
 
-                        <Form.Group controlId="password" className="my-3">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                value={password}
-                                className="fs-5"
-                                onChange={(event) => setPassword(event.target.value)}
-                            >
-                            </Form.Control>
-                        </Form.Group>
+                            <Form.Group controlId="password" className="my-3">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    value={password}
+                                    className="fs-5"
+                                    onChange={(event) => setPassword(event.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
 
-                        <Form.Group controlId="confirmPassword" className="my-3">
-                            <Form.Label>Confirm Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                value={confirmPassword}
-                                className="fs-5"
-                                onChange={(event) => setConfirmPassword(event.target.value)}
-                            >
-                            </Form.Control>
-                        </Form.Group>
+                            <Form.Group controlId="confirmPassword" className="my-3">
+                                <Form.Label>Confirm Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    value={confirmPassword}
+                                    className="fs-5"
+                                    onChange={(event) => setConfirmPassword(event.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
 
-                        <Button type="submit" variant="primary" className="mt-2">
-                            {loadingUpdateProfile ? <Loader update /> : 'Update'}
-                        </Button>
-                    </Form>
+                            <Button type="submit" variant="primary" className="mb-2 w-100">
+                                {loadingUpdateProfile ? <Loader update /> : 'Update'}
+                            </Button>
+                        </Form>
+                    </Card>
                 </Col>
-                <Col md={9}>
-                    <h2>Orders</h2>
-                    {isLoading ? (
-                        <Loader />
-                    ) : error ? (
-                        <Message variant="danger">
-                            {error?.data?.message || error.error}
-                        </Message>
-                    ) : (
-                        <Table striped responsive className="table-sm">
-                            <thead>
-                                <tr>
-                                    <th>TRANSACTION ID</th>
-                                    <th>DATE</th>
-                                    <th>TOTAL</th>
-                                    <th>PAID</th>
-                                    <th>DELIVERED</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {orders.map((order) => (
-                                    <tr key={order._id}>
-                                        <td>{order._id}</td>
-                                        <td>{formatDateAndTime(order.createdAt)}</td>
-                                        <td>${addCommas(order.totalPrice)}</td>
-                                        <td>
-                                            {order.isPaid ? (
-                                                // <>
-                                                //     <span>{formatDateAndTime(order.paidAt)} </span>
-                                                //     <FaCheck className="text-success mb-1" />
-                                                // </>
-                                                <FaCheck className="text-success fs-5" />
-                                            ) : (
-                                                <FaTimes className="text-danger fs-5" />
-                                            )}
-                                        </td>
-                                        <td>
-                                            {order.isDelievered ? (
-                                                // <>
-                                                //     <span>{formatDateAndTime(order.isDelievered)} </span>
-                                                //     <FaCheck className="text-success mb-1" />
-                                                // </>
-                                                <FaCheck className="text-success fs-5" />
-                                            ) : (
-                                                <FaTimes className="text-danger fs-5" />
-                                            )}
-                                        </td>
-                                        <td>
-                                            <LinkContainer to={`/order/${order._id}`}>
-                                                <FaExternalLinkAlt role="button" />
-                                            </LinkContainer>
-                                        </td>
+                {emptyOrdersList ? (
+                    <Col md={13} lg={4} className="mt-5 fs-1 text-center">
+                        <h3>No Recent Orders</h3>
+                        <h1>¯\_(ツ)_/¯</h1>
+                    </Col>
+                ) : (
+                    <Col md={13} lg={8} className="mt-2 text-center">
+                        <h3>Recent Orders</h3>
+                        {isLoading ? (
+                            <Loader />
+                        ) : error ? (
+                            <Message variant="danger">
+                                {error?.data?.message || error.error}
+                            </Message>
+                        ) : (
+                            <Table striped responsive className="table-sm">
+                                <thead>
+                                    <tr>
+                                        <th>TRANSACTION</th>
+                                        <th>DATE</th>
+                                        <th>TOTAL</th>
+                                        <th>PAID</th>
+                                        <th>DELIVERED</th>
+                                        <th></th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </Table>
-                    )}
-                </Col>
-            </Row>
+                                </thead>
+                                <tbody>
+                                    {orders.map((order) => (
+                                        <tr key={order._id}>
+                                            <td>{order._id}</td>
+                                            <td>{formatDateAndTime(order.createdAt)}</td>
+                                            <td>${addCommas(order.totalPrice)}</td>
+                                            <td>
+                                                {order.isPaid ? (
+                                                    // <>
+                                                    //     <span>{formatDateAndTime(order.paidAt)} </span>
+                                                    //     <FaCheck className="text-success mb-1" />
+                                                    // </>
+                                                    <FaCheck className="text-success fs-5" />
+                                                ) : (
+                                                    <FaTimes className="text-danger fs-5" />
+                                                )}
+                                            </td>
+                                            <td>
+                                                {order.isDelievered ? (
+                                                    // <>
+                                                    //     <span>{formatDateAndTime(order.isDelievered)} </span>
+                                                    //     <FaCheck className="text-success mb-1" />
+                                                    // </>
+                                                    <FaCheck className="text-success fs-5" />
+                                                ) : (
+                                                    <FaTimes className="text-danger fs-5" />
+                                                )}
+                                            </td>
+                                            <td>
+                                                <LinkContainer to={`/order/${order._id}`}>
+                                                    <FaExternalLinkAlt role="button" />
+                                                </LinkContainer>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        )}
+                    </Col>
+                )}
+            </Row >
         </>
     )
 }
