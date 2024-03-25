495c745583628ee9e67a8d3f82b48be4b072b23c
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2024-03-25 17:44:07 +0200

    updated

--- backend/server.js
+++ backend/server.js
@@ -18,24 +18,6 @@ const app = express()
 const port = process.env.PORT || 5000
 const __dirname = path.resolve() // set __dirname to current directory
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://ecommecre-shop.onrender.com')
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-    // Set to true if you need the website to include cookies in the requests sent to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true)
-
-    // Pass to next layer of middleware
-    next()
-})
-
 // body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
